b10223e1bc02a44afe85146d07db23452c5adf3d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-09-11 17:23:41 +0200

    finish hub with gc

--- src/hub.rs
+++ src/hub.rs
@@ -1,32 +1,76 @@
-use std::collections::VecDeque;
-use std::sync::Mutex;
-use std::collections::HashMap;
+use std::{
+    array,
+    collections::{hash_map::RandomState, HashMap, VecDeque},
+    hash::{BuildHasher, Hash, Hasher},
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
 use tokio::sync::Notify;
-use std::hash::Hash;
-use std::sync::Arc;
+use tokio::time::sleep;
+
+const NUM_SHARDS: usize = 64;
 
 const MAX_ITEMS: usize = 1024;
 
-pub struct Hub<K, V> {
-    shards: [Mutex<Shard<K, V>>; 8],
+pub struct Hub<S, R> {
+    random_state: RandomState,
+    shards: [Mutex<Shard<S, R>>; NUM_SHARDS],
 }
 
-impl<K, V> Hub<K, V> {
-    pub fn submit(selector: K, data: V) {
-
+impl<S: Hash + Eq + Clone, R> Hub<S, R> {
+    pub fn new() -> Hub<S, R> {
+        Hub {
+            random_state: RandomState::new(),
+            shards: array::from_fn(|_| Mutex::new(Shard::new())),
+        }
     }
 
-    pub async fn acquire(selector: K) -> V {
-        todo!()
+    pub fn submit(&self, selector: S, data: R) {
+        let shard = self.shard(&selector);
+        shard.lock().unwrap().submit(selector, data);
+    }
+
+    pub async fn acquire(&self, selector: S) -> R {
+        let shard = self.shard(&selector);
+        loop {
+            match shard.lock().unwrap().acquire(selector.clone()) {
+                Ok(item) => return item,
+                Err(signal) => signal.notified().await,
+            }
+        }
+    }
+
+    fn shard(&self, selector: &S) -> &Mutex<Shard<S, R>> {
+        let mut hasher = self.random_state.build_hasher();
+        selector.hash(&mut hasher);
+        &self.shards[hasher.finish() as usize % NUM_SHARDS]
     }
 }
 
-struct Shard<K, V> {
-    map: HashMap<K, Queue<V>>,
+impl<S, R: IsValid> Hub<S, R> {
+    pub async fn garbage_collect(&self) {
+        loop {
+            for shard in &self.shards {
+                shard.lock().unwrap().garbage_collect();
+                sleep(Duration::from_secs(10)).await;
+            }
+        }
+    }
 }
 
-impl<K: Eq + Hash, V> Shard<K, V> {
-    fn submit(&mut self, selector: K, data: V) {
+struct Shard<S, R> {
+    map: HashMap<S, Queue<R>>,
+}
+
+impl<S: Eq + Hash, R> Shard<S, R> {
+    fn new() -> Shard<S, R> {
+        Shard {
+            map: HashMap::new(),
+        }
+    }
+
+    fn submit(&mut self, selector: S, data: R) {
         let entry = self.map.entry(selector).or_default();
         if entry.inner.len() < MAX_ITEMS {
             entry.inner.push_back(data);
@@ -34,22 +78,38 @@ impl<K: Eq + Hash, V> Shard<K, V> {
         }
     }
 
-    fn acquire(&mut self, selector: K) -> Result<V, Arc<Notify>> {
+    fn acquire(&mut self, selector: S) -> Result<R, Arc<Notify>> {
         let entry = self.map.entry(selector).or_default();
-        entry.inner.pop_front().ok_or_else(|| Arc::clone(&entry.signal))
+        entry
+            .inner
+            .pop_front()
+            .ok_or_else(|| Arc::clone(&entry.signal))
     }
 }
 
-struct Queue<V> {
-    signal: Arc<Notify>,
-    inner: VecDeque<V>,
+impl<S, R: IsValid> Shard<S, R> {
+    fn garbage_collect(&mut self) {
+        self.map.retain(|_, queue| {
+            queue.inner.retain(|item| item.is_valid());
+            !queue.inner.is_empty()
+        });
+    }
 }
 
-impl<V> Default for Queue<V> {
-    fn default() -> Queue<V> {
+struct Queue<R> {
+    signal: Arc<Notify>,
+    inner: VecDeque<R>,
+}
+
+impl<R> Default for Queue<R> {
+    fn default() -> Queue<R> {
         Queue {
             signal: Arc::new(Notify::new()),
             inner: VecDeque::new(),
         }
     }
+}
+
+pub trait IsValid {
+    fn is_valid(&self) -> bool;
 }
