412c42b1e8367c030065993ecc7da1a531559b51
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-09-28 18:03:22 +0200

    deal with empty responses

--- test/serve.sh
+++ test/serve.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 while true; do
-    echo Working ...
+    echo Waiting ...
     http --print b POST http://localhost:9666/api/external-engine/work providerSecret=reallyReallySecret > /tmp/job
-    job_id=$(cat /tmp/job | jq -r .id)
-    echo $job_id
-    stockfish go depth 20 | http --chunked POST "http://localhost:9666/api/external-engine/work/$job_id"
+    job_id=$(cat /tmp/job | jq -r .id || echo "")
+    if [ -n "$job_id" ]; then
+        echo Now analysing ...
+        stockfish go depth 20 | http --chunked POST "http://localhost:9666/api/external-engine/work/$job_id"
+    fi
 done
