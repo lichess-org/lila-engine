64c95a6b7a7d2333987916a7b0e9b7db440acb8f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-01 18:18:32 +0200

    tweak client

--- test/analyse.sh
+++ test/analyse.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-http --stream POST https://engine.lichess.ovh/api/external-engine/eei_iB8ZF5UtlhC4/analyse clientSecret=ees_2YwuAk2CuO6ERL8a work[sessionId]=hi work[threads]:=1 work[hashMib]:=16 work[maxDepth]:=22 work[multiPv]:=5 work[variant]=standard work[initialFen]="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" work[moves]:=[]
+http --stream POST http://localhost:9666/api/external-engine/eei_JMzld9A6gzQT/analyse clientSecret=ees_wOyQHDuV3hV82x2Y work[sessionId]=hi work[threads]:=1 work[hashMib]:=16 work[maxDepth]:=22 work[multiPv]:=5 work[variant]=standard work[initialFen]="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" work[moves]:=[]
--- test/serve.sh
+++ test/serve.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
 while true; do
-    echo Waiting ...
+    echo "# Waiting ..."
     job=$(http --print b POST http://localhost:9666/api/external-engine/work providerSecret=reallyReallySecret)
     if [ -n "$job" ]; then
         job_id=$(echo "$job" | jq -r .id)
-        echo "Now analysing $job_id ..."
-        stockfish go depth 20 | http --chunked POST "http://localhost:9666/api/external-engine/work/$job_id"
+        depth=$(echo "$job" | jq -r .work.maxDepth)
+        command="go depth $depth"
+        echo "$command"
+        cat <(echo "$command") - | stockfish | http --chunked --stream POST "http://localhost:9666/api/external-engine/work/$job_id"
     fi
 done
