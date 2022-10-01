47650b7bdb491229022bfe5179d0f01f699fdf1a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-01 18:08:47 +0200

    tweak serve.sh

--- test/serve.sh
+++ test/serve.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 while true; do
     echo Waiting ...
-    http --print b POST http://localhost:9666/api/external-engine/work providerSecret=reallyReallySecret > /tmp/job
-    job_id=$(cat /tmp/job | jq -r .id || echo "")
-    if [ -n "$job_id" ]; then
-        echo Now analysing ...
+    job=$(http --print b POST http://localhost:9666/api/external-engine/work providerSecret=reallyReallySecret)
+    if [ -n "$job" ]; then
+        job_id=$(echo "$job" | jq -r .id)
+        echo "Now analysing $job_id ..."
         stockfish go depth 20 | http --chunked POST "http://localhost:9666/api/external-engine/work/$job_id"
     fi
 done
