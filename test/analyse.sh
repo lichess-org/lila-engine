c70bbe41c90663a7523b47a61790543c2912451f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-01 18:25:26 +0200

    let test script analyse the actual position

--- test/analyse.sh
+++ test/analyse.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-http --stream POST http://localhost:9666/api/external-engine/eei_JMzld9A6gzQT/analyse clientSecret=ees_wOyQHDuV3hV82x2Y work[sessionId]=hi work[threads]:=1 work[hashMib]:=16 work[maxDepth]:=22 work[multiPv]:=5 work[variant]=standard work[initialFen]="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" work[moves]:=[]
+http --stream POST http://localhost:9666/api/external-engine/eei_JMzld9A6gzQT/analyse clientSecret=ees_wOyQHDuV3hV82x2Y work[sessionId]=hi work[threads]:=1 work[hashMib]:=16 work[maxDepth]:=22 work[multiPv]:=5 work[variant]=standard work[initialFen]="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" work[moves]:='["e2e4","d7d5"]'
--- test/serve.sh
+++ test/serve.sh
@@ -5,8 +5,10 @@ while true; do
     if [ -n "$job" ]; then
         job_id=$(echo "$job" | jq -r .id)
         depth=$(echo "$job" | jq -r .work.maxDepth)
-        command="go depth $depth"
-        echo "$command"
-        cat <(echo "$command") - | stockfish | http --chunked --stream POST "http://localhost:9666/api/external-engine/work/$job_id"
+        initial_fen=$(echo "$job" | jq -r .work.initialFen)
+        moves=$(echo "$job" | jq -r '.work.moves | join(" ")')
+        command="position fen $initial_fen moves $moves\ngo depth $depth"
+        echo -e "$command"
+        cat <(echo -e "$command") - | stockfish | http --chunked POST "http://localhost:9666/api/external-engine/work/$job_id"
     fi
 done
