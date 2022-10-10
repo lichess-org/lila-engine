6dabeceda58ef98ec12db21b0b8607bc9e61235e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-10 22:50:11 +0200

    create more powerful engine in test/create.sh

--- test/create.sh
+++ test/create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-http -A bearer -a $PAT POST http://localhost:9663/api/external-engine name="Stockfish 15" maxThreads=1 maxHash=16 providerSecret=reallyReallySecret
+http -A bearer -a $PAT POST http://localhost:9663/api/external-engine name="Stockfish 15" maxThreads=12 maxHash=4096 providerSecret=reallyReallySecret
