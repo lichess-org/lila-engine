922b9b33b588951ab258c6550858eb7258ed3844
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-11 22:47:53 +0200

    update test script

--- test/serve.sh
+++ test/serve.sh
@@ -4,10 +4,9 @@ while true; do
     job=$(http --print b POST http://localhost:9666/api/external-engine/work providerSecret=reallyReallySecret)
     if [ -n "$job" ]; then
         job_id=$(echo "$job" | jq -r .id)
-        depth=$(echo "$job" | jq -r .work.maxDepth)
         initial_fen=$(echo "$job" | jq -r .work.initialFen)
         moves=$(echo "$job" | jq -r '.work.moves | join(" ")')
-        command="position fen $initial_fen moves $moves\ngo depth $depth"
+        command="position fen $initial_fen moves $moves\ngo depth 25"
         echo -e "$command"
         cat <(echo -e "$command") - | stockfish | http --chunked POST "http://localhost:9666/api/external-engine/work/$job_id"
     fi
