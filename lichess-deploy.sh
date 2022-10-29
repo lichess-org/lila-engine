9932ddb714478698dc0b344b8dfde39d814f5b8b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-29 20:55:26 +0200

    temporarily update deploy script

--- lichess-deploy.sh
+++ lichess-deploy.sh
@@ -2,4 +2,4 @@
 cargo +stable build --release --target x86_64-unknown-linux-musl
 ssh "root@$1.lichess.ovh" mv /usr/local/bin/lila-engine /usr/local/bin/lila-engine.bak || (echo "first deploy on this server? set up service and comment out this line" && false)
 scp ./target/x86_64-unknown-linux-musl/release/lila-engine "root@$1.lichess.ovh":/usr/local/bin/lila-engine
-#ssh "root@$1.lichess.ovh" systemctl restart lila-engine lila-engine-tls
+ssh "root@$1.lichess.ovh" systemctl restart lila-engine-tls
